4d8fd19014fd3c4ecf81b27e889f90acd5ad2b98
Admin <Netamamann@gmail.com>
2023-03-28 13:17:10 +0300

    orgenize the code

--- client/src/pages/WhoAreYouProfile.js
+++ client/src/pages/WhoAreYouProfile.js
@@ -5,6 +5,12 @@ import { useNavigate, Navigate } from "react-router-dom";
 import looker_img from '../images/looker_img.png';
 import welcomer_img from '../images/welcomer_img.png';
 
+const btnstyle = {
+    // margin: "8px 0",
+    background: "#4F4E51",
+    height: "50px",
+    color: "#D0D2D8",
+  };
 
 const WhoAreYouProfile= () =>{
     const navigate = useNavigate();
@@ -82,13 +88,19 @@ const WhoAreYouProfile= () =>{
         </Button>
         </Box>
     </Box>
+
+
+    <Box container="true" sx={{display:'flex',flexWrap:"warp",  width: "auto", marginLeft:"auto", marginRight:'auto', marginTop:3}} >
+    <Box item="true" xs={4} sx={{width: 400, marginLeft:"auto", marginRight:'auto'}} >
     <Button 
     variant="contained"
     onClick={()=>{navigate("/create-profile")}}
+    style={btnstyle}
     > 
         Continue
     </Button>
-    
+    </Box>
+    </Box>
     </>
     
     );
