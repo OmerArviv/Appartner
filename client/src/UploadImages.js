7215f0bcc98cb67c508bf3b721cf3102d7e12c6d
Admin <Netamamann@gmail.com>
2023-04-18 17:48:58 +0300

    user can upload link to image

--- client/src/UploadImages.js
+++ client/src/UploadImages.js
@@ -1,36 +1,39 @@
 
-import {Box, Card, CardActionArea, CardMedia} from "@mui/material";
-import { useState } from "react";
+import {Box, Card,} from "@mui/material";
+import DialogImage from "./components/DialogImage";
 
 const UploadImages= ()=>{
-    const [image1, setImage1] = useState("");
-    const [image2, setImage2] = useState("");
-    const [image3, setImage3] = useState("");
-    const [image4, setImage4] = useState("");
-
-    function image1Handler(event) {
-        console.log(event.target.value);
-        setImage1(event.target.value);
-    }
-
-
 
     return(
+        <>
+        {/* <DialogImage></DialogImage> */}
         <Box item="true" xs={4} sx={{display:'flex',flexWrap: 'wrap', width: "auto", marginLeft:"auto", marginRight:'auto'}} >
         <Box item="true" xs={4} sx={{ width: "auto", marginLeft:"auto", marginRight:'auto'}} >
+        
             <Card  xs={2} sx={{height: 'fit-content', width: 'fit-content', marginLeft:"auto", marginRight:'auto', marginRight:10}}>
-                <CardActionArea>
+            <DialogImage/>
+            </Card>
+
+            <Card  xs={2} sx={{height: 'fit-content', width: 'fit-content', marginRight:'auto', marginTop:1}}>
+            <DialogImage/>
+            </Card>
+           
+            {/* <Card  xs={2} sx={{height: 'fit-content', width: 'fit-content', marginLeft:"auto", marginRight:'auto', marginRight:10}}>
+                <CardActionArea  onClick={image1Handler}>
+
                     <CardMedia
                     component="img"
                     height="200"
                     width="200"
                     image="https://i0.wp.com/getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg"
                     alt="upload image 1"
-                    onClick={image1Handler}>
+                    // onClick={image1Handler}
+                    >
                     </CardMedia>
                 </CardActionArea>
-            </Card>
-            <Card  xs={2} sx={{height: 'fit-content', width: 'fit-content', marginRight:'auto', marginTop:1}}>
+            </Card> */}
+            
+            {/* <Card  xs={2} sx={{height: 'fit-content', width: 'fit-content', marginRight:'auto', marginTop:1}}>
                 <CardActionArea>
                     <CardMedia
                     component="img"
@@ -40,12 +43,13 @@ const UploadImages= ()=>{
                     alt="upload image 3">
                     </CardMedia>
                 </CardActionArea>
-            </Card>
+            </Card> */}
            </Box>
 
             <Box item="true" xs={4} sx={{ width: "auto", marginLeft:"auto", marginRight:'auto'}} >
             <Card  xs={2} sx={{height: 'fit-content', width: 'fit-content', marginLeft:"auto", marginRight:'auto',}}>
-                <CardActionArea>
+              <DialogImage/>
+                {/* <CardActionArea>
                     <CardMedia
                     component="img"
                     height="200"
@@ -53,10 +57,11 @@ const UploadImages= ()=>{
                     image="https://i0.wp.com/getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg"
                     alt="upload image 2">
                     </CardMedia>
-                </CardActionArea>
+                </CardActionArea> */}
             </Card>
             <Card  xs={2} sx={{height: 'fit-content', width: 'fit-content', marginRight:'auto', marginTop:1}}>
-                <CardActionArea>
+              <DialogImage/>
+                {/* <CardActionArea>
                     <CardMedia
                     component="img"
                     height="200"
@@ -64,12 +69,12 @@ const UploadImages= ()=>{
                     image="https://i0.wp.com/getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg"
                     alt="upload image 4">
                     </CardMedia>
-                </CardActionArea>
+                </CardActionArea> */}
             </Card>
-    
         </Box>
-
         </Box>
+       
+        </>
     );
 }
 
