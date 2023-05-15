55671ec3e8dd41b1a91d65aa9126f9b110c4c1bf
OmerNadam91 <omer.n@netspark.com>
2023-05-15 21:40:32 +0300

    add best matches framework

--- server/Controller/RoomateRequestController.js
+++ server/Controller/RoomateRequestController.js
@@ -62,4 +62,18 @@ router
     return response.status(204).send(null);
   });
 
+  router.route("/getBestMatches").post(async (request, response) => {
+    const { data } = request.body;
+    if (!data) {
+      return response.status(403).send({});
+    }
+  
+    const dataJson = await RoomateRequestService.getMatches(data);
+  
+    if (dataJson) {
+      return response.status(200).json(dataJson);
+    }
+    return response.status(200).send(null);
+  });
+
 module.exports = router;
